f5a78db5135c5585778f0a5d2b69e81237ba3401
charlestechnologie <charleskurbitsh657@gmail.com>
2022-10-24 22:08:39 -0400

    b

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Contactez-nous","Rejoignez notre liste de diffusion!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
